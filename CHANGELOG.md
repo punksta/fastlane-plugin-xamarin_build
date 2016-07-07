768ed1dde9daf167841f144f36318d0a78f389e9
punksta <skinnystas@gmail.com>
2016-07-07 11:37:50 +0300

    ruby 2.0 support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.3
+> fix gem file
+
 ## 0.1.2
 > fix bugs, added ruby 2.0 support
 
--- lib/fastlane/plugin/xamarin_build/version.rb
+++ lib/fastlane/plugin/xamarin_build/version.rb
@@ -1,6 +1,6 @@
 module Fastlane
   module XamarinBuild
-    VERSION = "0.1.2"
+    VERSION = "0.1.3"
   end
 end
 
